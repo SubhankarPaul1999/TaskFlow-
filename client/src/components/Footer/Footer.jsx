bf56ade31110833b07db1a8276faabdb5d1f88a7
SubhankarPaul1999 <88609918+SubhankarPaul1999@users.noreply.github.com>
2025-03-29 22:13:15 +0530

    Initial commit

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -8,18 +8,7 @@ const Footer = () => {
         <div className='footer container-fluid p-4 text-white'>
             <div className="container d-flex justify-content-between align-items-center">
                 <div className="footer-title">
-                    <Link to='/'>ListFlow</Link>
-                </div>
-                <div className='footer-copyright'>
-                    Developed by <a href="https://pranabks.vercel.app" target='_blank' rel="noreferrer">Pranab KS</a>
-                </div>
-                <div className='social-links'>
-                    <a href="https://github.com/PranabKumarSahoo/listflow" target='_blank' rel="noreferrer">
-                        GitHub <FiArrowUpRight />
-                    </a>
-                    <a href="https://twitter.com/PranavvvKS" target='_blank' rel="noreferrer">
-                        Twitter <FiArrowUpRight />
-                    </a>
+                    <Link to='/'>TaskFlow</Link>
                 </div>
             </div>
         </div >
--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
-const url = `mongodb+srv://pranab90:pranab90783@cluster0.0opmf0k.mongodb.net/`;
-
+// const url = `mongodb+srv://pranab90:pranab90783@cluster0.0opmf0k.mongodb.net/`;
+// const url1 = `mongodb://127.0.0.1:27017/TodoDB`;
+const MONGO_URI = process.env.MONGO_URI;
 const connectDatabase = async (req, res) => {
     try {
         await mongoose
-            .connect(url)
+            .connect(MONGO_URI)
             .then(() => {
                 console.log("Database Connected!!");
             });
